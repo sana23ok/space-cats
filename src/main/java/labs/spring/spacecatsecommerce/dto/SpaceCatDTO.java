35cefad853155937b1d01faa948fae8c51eac227
Oksana Hordych <oksanahordych@gmail.com>
2024-11-19 23:25:54 +0200

    @Data replaced with @Value

--- src/main/java/labs/spring/spacecatsecommerce/dto/SpaceCatDTO.java
+++ src/main/java/labs/spring/spacecatsecommerce/dto/SpaceCatDTO.java
@@ -5,9 +5,10 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
 
-@Data
+@Value
 @Builder
 @Jacksonized
 public class SpaceCatDTO {
